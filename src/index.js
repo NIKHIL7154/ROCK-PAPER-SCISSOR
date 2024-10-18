437d4dfd3290483c40e33f706f33da2d22f73396
NIKHIL THAKUR <nikhilthakurq@gmail.com>
2024-10-18 18:47:12 +0530

    Backend uploaded to ec2 AWS

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import io from 'socket.io-client'
 
 import App from './App';
 
+const Host=process.env.REACT_APP_META_SERVER_URI
 
-const soc = io.connect("https://rockserver.adaptable.app");
+const soc = io.connect(Host);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
